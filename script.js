eb68a2c371780d6ccd7d56db665eba91f119ecaa
Yoav <ybd3131@gmail.com>
2024-08-14 16:44:54 +0300

    updated Frame only route(box frame)

--- script.js
+++ script.js
@@ -125,7 +125,7 @@ const boxFrameOriginalsPriceTable = {
 };
 
 const addonPriceTable = {
-    'Anti-Reflective Glass': { '30x20': 35, '40x30': 105, '60x40': 220, '70x50': 330, '90x60': 500, '100x70': 650, '120x80': 900, '130x90': 1100, '120x100': 1100, '150x100': 1400 },
+    'Anti-Reflective Glass': { '30x20': 90, '40x30': 160, '60x40': 280, '70x50': 400, '90x60': 590, '100x70': 760, '120x80': 1040, '130x90': 1260, '120x100': 1260, '150x100': 1610 },
     'Passepartout Box': { '30x20': 0, '40x30': 0, '60x40': 0, '70x50': 0, '90x60': 50, '100x70': 80, '120x80': 120, '130x90': 220, '150x100': 330 },
     'Passepartout Aluminum': { '30x20': 50, '40x30': 70, '60x40': 120, '70x50': 160, '90x60': 250, '100x70': 330, '120x80': 400, '130x90': 480, '150x100': 620 },
     'Museum-Quality Mat': { '30x20': 0, '40x30': 0, '60x40': 0, '70x50': 0, '90x60': 50, '100x70': 80, '120x80': 160, '130x90': 220, '120x100': 220, '150x100': 330 }
@@ -144,8 +144,8 @@ const translations = {
     'Aluminium Frame': 'מסגור אלומיניום',
     'Floating Frame For Paintings': 'מסגור צף לציורים',
     'Light Box(Includes Printing)': 'קופסת אור (כולל הדפסה) ',
-    'KAPA White (Line)': 'קאפה לבנה (Line)',
-    'KAPA Grey (Fix)': 'קאפה אפורה (Fix)',
+    'KAPA White (Line)': 'קאפה לבן (Line)',
+    'KAPA Grey (Fix)': 'קאפה אפור (Fix)',
     'Dibond': 'דיבונד',
     'Diasec': 'דיאסק',
     'Glossy': 'מבריק',
@@ -345,7 +345,7 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('resetButton').innerHTML = 'איפוס <svg width="18" height="21" viewBox="0 0 18 21" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 21C7.75 21 6.57917 20.7625 5.4875 20.2875C4.39583 19.8125 3.44583 19.1708 2.6375 18.3625C1.82917 17.5542 1.1875 16.6042 0.7125 15.5125C0.2375 14.4208 0 13.25 0 12H2C2 13.95 2.67917 15.6042 4.0375 16.9625C5.39583 18.3208 7.05 19 9 19C10.95 19 12.6042 18.3208 13.9625 16.9625C15.3208 15.6042 16 13.95 16 12C16 10.05 15.3208 8.39583 13.9625 7.0375C12.6042 5.67917 10.95 5 9 5H8.85L10.4 6.55L9 8L5 4L9 0L10.4 1.45L8.85 3H9C10.25 3 11.4208 3.2375 12.5125 3.7125C13.6042 4.1875 14.5542 4.82917 15.3625 5.6375C16.1708 6.44583 16.8125 7.39583 17.2875 8.4875C17.7625 9.57917 18 10.75 18 12C18 13.25 17.7625 14.4208 17.2875 15.5125C16.8125 16.6042 16.1708 17.5542 15.3625 18.3625C14.5542 19.1708 13.6042 19.8125 12.5125 20.2875C11.4208 20.7625 10.25 21 9 21Z" fill="#1C1B1F"/></svg>';
         document.querySelector('.total-cost-label').textContent = 'עלות כוללת:';
         document.getElementById('totalCost').textContent = '0 ₪';
-        document.querySelector('.footnote p').innerHTML = 'המחירים המפורטים הם עבור גדלים סטנדרטיים. לקבלת הצעת מחיר עבור גדלים גדולים ומיוחדים, אנא צרו איתנו קשר. <span style="color:black; font-weight:bold;">מינימום הזמנה של 750 ש"ח.</span>';
+        document.querySelector('.footnote p').innerHTML = 'המחירים המפורטים הם עבור גדלים סטנדרטיים. לקבלת הצעת מחיר עבור גדלים מותאמים אישית, אנא צרו איתנו קשר. <span style="color:black; font-weight:bold;">מינימום הזמנה של 750 ש"ח.</span>';
         document.getElementById('customWidth').placeholder = 'רוחב';
         document.getElementById('customHeight').placeholder = 'גובה';
         document.getElementById('customSizeButton').textContent = 'OK';
@@ -356,7 +356,7 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('resetButton').innerHTML = 'Reset <svg width="18" height="21" viewBox="0 0 18 21" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 21C7.75 21 6.57917 20.7625 5.4875 20.2875C4.39583 19.8125 3.44583 19.1708 2.6375 18.3625C1.82917 17.5542 1.1875 16.6042 0.7125 15.5125C0.2375 14.4208 0 13.25 0 12H2C2 13.95 2.67917 15.6042 4.0375 16.9625C5.39583 18.3208 7.05 19 9 19C10.95 19 12.6042 18.3208 13.9625 16.9625C15.3208 15.6042 16 13.95 16 12C16 10.05 15.3208 8.39583 13.9625 7.0375C12.6042 5.67917 10.95 5 9 5H8.85L10.4 6.55L9 8L5 4L9 0L10.4 1.45L8.85 3H9C10.25 3 11.4208 3.2375 12.5125 3.7125C13.6042 4.1875 14.5542 4.82917 15.3625 5.6375C16.1708 6.44583 16.8125 7.39583 17.2875 8.4875C17.7625 9.57917 18 10.75 18 12C18 13.25 17.7625 14.4208 17.2875 15.5125C16.8125 16.6042 16.1708 17.5542 15.3625 18.3625C14.5542 19.1708 13.6042 19.8125 12.5125 20.2875C11.4208 20.7625 10.25 21 9 21Z" fill="#1C1B1F"/></svg>';
         document.querySelector('.total-cost-label').textContent = 'Total Cost:';
         document.getElementById('totalCost').textContent = '0 NIS';
-        document.querySelector('.footnote p').innerHTML = 'Prices listed are for standard sizes. For large and special sizes, please contact us for a quote. <span style="color:black;">Minimum order of 750 NIS.</span>';
+        document.querySelector('.footnote p').innerHTML = 'Prices listed are for standard sizes. For custom sizes, please contact us for a quote. <span style="color:black;">Minimum order of 750 NIS.</span>';
         document.getElementById('customWidth').placeholder = 'Width';
         document.getElementById('customHeight').placeholder = 'Height';
         document.getElementById('customSizeButton').textContent = 'OK';
@@ -401,7 +401,6 @@ function translateToHebrew() {
     document.querySelector('h2').textContent = 'מחשבון מחירים';
     document.getElementById('labelProductType').textContent = 'מוצר';
     document.getElementById('labelPaperType').textContent = 'נייר';
-    document.getElementById('labelPrintSize').textContent = 'הדפסה';
     document.getElementById('labelFrameType').textContent = 'מסגרת';
     document.getElementById('labelMountType').textContent = 'הדבקה';
     document.getElementById('labelDiasecType').textContent = 'דיאסק';
@@ -659,22 +658,13 @@ document.getElementById('customSizeButton').addEventListener('click', () => {
     const printSizeSelect = document.getElementById('printSize');
     const minSize = 10;
     const minArea = 600;
-    const maxWidth = 180;
-    const maxHeight = 120;
 
     if (customWidth >= minSize && customHeight >= minSize && (customWidth * customHeight) >= minArea) {
-        if ((customWidth* customHeight) > (maxWidth * maxHeight)) {
-            const message = getLanguage() === 'he' 
-                ? `למידות גדולות יש ליצור איתנו קשר`
-                : `For large sizes, please contact us`;
-            showPopup(message);
-        } else {
-            addCustomSizeToTables(customWidth, customHeight);
-            populatePrintSizes();  // Ensure the custom size is added to the dropdown
-            printSizeSelect.value = `${customWidth}x${customHeight}`;  // Set the custom size value
-            printSizeSelect.dispatchEvent(new Event('change'));  // Trigger change event to update the UI
-            updateSummary();
-        }
+        addCustomSizeToTables(customWidth, customHeight);
+        populatePrintSizes();  // Ensure the custom size is added to the dropdown
+        printSizeSelect.value = `${customWidth}x${customHeight}`;  // Set the custom size value
+        printSizeSelect.dispatchEvent(new Event('change'));  // Trigger change event to update the UI
+        updateSummary();
     } else {
         const message = getLanguage() === 'he' 
             ? `אנא ודא שהמידות הן לפחות ${minSize}x${minSize} והשטח הוא לפחות ${minArea} סמ"ר.`
@@ -737,7 +727,7 @@ function showOptions() {
         printSizeOptions.classList.remove('hidden');
         populatePrintSizes();
     }
-
+    
     updateSummary(); // Ensure summary is updated after product type selection
 }
 
@@ -752,9 +742,21 @@ function populatePrintSizes() {
     const productType = document.getElementById('productType').value;
     const paperType = document.getElementById('paperType').value;
     const printSizeSelect = document.getElementById('printSize');
+    const labelPrintSize = document.getElementById('labelPrintSize');
     const lang = document.documentElement.lang;
 
-    printSizeSelect.innerHTML = `<option value="" disabled selected>${lang === 'he' ? 'בחר גודל הדפסה' : 'Select Print Size'}</option>`;
+
+
+    // Change the label text based on the product type
+    if (productType === 'Frame Only') {
+        labelPrintSize.textContent = lang === 'he' ? 'מידה' : 'Size';
+        printSizeSelect.innerHTML = `<option value="" disabled selected>${lang === 'he' ? 'בחר מידה למסגרת' : 'Select Frame Size'}</option>`;
+    } else {
+        labelPrintSize.textContent = lang === 'he' ? 'הדפסה' : 'Size';
+        printSizeSelect.innerHTML = `<option value="" disabled selected>${lang === 'he' ? 'בחר גודל הדפסה' : 'Select Print Size'}</option>`;
+    }
+
+
 
     if (productType === 'Frame Only') {
         for (const size in dibondNoBackFramePriceTable) {
@@ -982,6 +984,9 @@ function showFrameOptions() {
     }else if(frameType === 'Aluminium Frame'){
         antiReflectiveGlassPrice = addonPriceTable['Anti-Reflective Glass'][printSize];
         passepartoutPrice = addonPriceTable['Passepartout Aluminum'][printSize];
+    }else if(frameType === 'Box Frame for Originals'){
+        antiReflectiveGlassPrice = addonPriceTable['Anti-Reflective Glass'][printSize];
+        passepartoutPrice = addonPriceTable['Passepartout Box'][printSize];
     }
     
     addonOptions.innerHTML += `
@@ -1001,7 +1006,11 @@ function showFrameOptions() {
             if (frameType !== 'Floating Frame For Paintings' && frameType !== 'Light Box(Includes Printing)'  && frameType !== 'Stretcher Frame' && frameType !== 'Box Frame for Originals') {
                 mountOptions.classList.remove('hidden');
                 populateMountTypes();  // Ensure mount types are populated based on selected frame type
-            } else {
+            } else if(frameType === 'Box Frame for Originals'){
+                addonOptions.classList.remove('hidden');
+                quantityOptions.classList.remove('hidden');
+            }
+            else {
                 quantityOptions.classList.remove('hidden');
             }
             updateSummary();
@@ -1032,13 +1041,23 @@ function updateSummary() {
         { id: 'diasecType', label: { en: 'Diasec Type (Acrylic Facemount)', he: 'סוג דיאסק (פרספקס קידמי)' } },
         { id: 'frameType', label: { en: 'Frame Type', he: 'סוג מסגור' } },
     ];
+    elements.forEach(element => {
+        if (element.id === 'printSize') {
+            if (document.getElementById('productType').value === 'Frame Only') {
+                element.label.he = 'מידת המסגרת';
+                element.label.en = 'Frame Size';
+            } else {
+                element.label.he = 'גודל הדפסה';
+                element.label.en = 'Print Size';
 
+            }
+        }
+    });
     const summaryText = document.getElementById('summaryText');
     summaryText.innerHTML = ''; // Clear previous content
 
     let totalCost = 0;
     let lang = document.documentElement.lang;
-    let productType = document.getElementById('productType').value;
     let paperType = document.getElementById('paperType').value;
     let printSize = document.getElementById('printSize').value;
     let mountType = document.getElementById('mountType').value;
